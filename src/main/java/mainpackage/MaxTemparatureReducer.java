130cbee73e2c0cd1d31cc87d15ec2841229df543
UdaySagar <uday.sagar.panjala@gmail.com>
2017-04-06 23:45:54 -0700

    Initial commit

---
+++ src/main/java/mainpackage/MaxTemparatureReducer.java
@@ -0,0 +1,20 @@
+package mainpackage;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * Created by User2 on 4/6/17.
+ */
+class MaxTemperatureReducer
+        extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int maxValue = Integer.MIN_VALUE; for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue)); }
+}
